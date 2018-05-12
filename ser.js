bd724ea77f4905b9cee3a0eaa09ac169ac4e18bd
Gang Zhang <gzhangx@hotmail.com>
2018-05-11 23:20:24 -0400

    calculating

--- ser.js
+++ ser.js
@@ -1,8 +1,6 @@
-console.log(calculateCorr([0xe8,0x03],0,0)/Math.PI*180)
-console.log(21.8*(155.3-1000)/(155.3*1000))
-return;
 
 const SerialPort = require('serialport');
+const fs = require('fs');
 const port = new SerialPort('com3', {
   baudRate: 128000 
 });
@@ -14,10 +12,8 @@ port.on('error', function(err) {
   console.log('Error: ', err.message);
 })
 
-function calculateCorr(data, i, off) {
-    const at = off+(i*2);
-    const dist = data[at] | (data[at+1] << 8);
-    console.log("dist=" + dist);
+function calculateCorr(dist) {
+    if (dist === 0) return 0;
     const corr = Math.atan(21.8*(155.3-dist)/(155.3*dist));
     return corr;
 }
@@ -27,11 +23,20 @@ port.on('data', function (data) {
   if (data[0] === 0xaa && data[1] === 0x55) {
      const fsa = (data[4] | (data[5] << 8))/128;
      const lsa = (data[6] | (data[7] << 8))/128;
-     const lsn = data[3];
+     const lsn = data[3] - 1;
      console.log(`${lsn} fs ${fsa} lsa ${lsa}`);
-     for (let i = 0; i < lsn;i++) {
+     const lenFsa = data[0] | (data[1] <<8);
+     const anglefsa = (fsa*Math.PI/180) + calculateCorr(lenFsa);
+     const diffAng = (lsa - fsa)*Math.PI/180;
+     for (let i = 0; i <= lsn;i++) {
          const len = data[i*2] | (data[i*2+1] <<8);
-
+         const ai = (diffAng/lsn*i) + anglefsa + calculateCorr(len);
+         console.log(`len=${len} ang=${ai*180/Math.PI}`);
+         const x = Math.cos(ai)*len;
+         const y = Math.sin(ai)*len;
+         fs.appendFile('data.txt',`${x} ${y}\r\n`,err=>{
+             if (err)console.log(err);
+         });
      }
   }
 });
